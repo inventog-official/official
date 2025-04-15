1219b84ecbb8c8b3aaa3b2f9bed53e2dceda69b8
gunavarma <gunavarma0000@gmail.com>
2025-04-15 08:46:38 +0530

    Comment out WhatsApp floating button in the contact component

--- components/floating-buttons.tsx
+++ components/floating-buttons.tsx
@@ -15,14 +15,14 @@ export default function FloatingButtons() {
   }, []);
   return (
     <div className="fixed bottom-6 left-6 z-50 flex flex-col gap-3">
-      <a
+      {/* <a
         href="https://wa.me/+917845931744"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300"
       >
         <MessageSquare className="w-5 h-5" />
-      </a>
+      </a> */}
 
       <a
         href="tel:+917845931744"
